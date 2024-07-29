7bd0ef0dd6ff86b13e467284c559807ea6ffc342
omodtung <dothedung131@gmail.com>
2024-07-29 09:19:48 +0700

    userinfo take from useUserForm

--- src/components/list/userInfo/userInfo.jsx
+++ src/components/list/userInfo/userInfo.jsx
@@ -1,10 +1,12 @@
 import './userInfo.css'
+import { useUserStore } from '../../../lib/userStore';
 const UserInfo = () => {
+  const { currentUser} = useUserStore();
   return (
     <div className="userInfo">
       <div className="user">
-        <img src="./avatar.png"></img>
-        <h2> The Tung</h2>
+      <img src={currentUser.avatar || "./avatar.png"} alt="" />
+        <h2>{currentUser.username}</h2>
       </div>
       <div className="icons">
 <img src="./more.png"></img>

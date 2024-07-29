ba2ad323d7270e22de0c0267514af8407b9561bc
omodtung <dothedung131@gmail.com>
2024-07-29 09:19:16 +0700

    Login with Fire base

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -6,14 +6,17 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from "firebase/auth";
-import { auth,db } from "../../lib/firebase";
+import { auth, db } from "../../lib/firebase";
 import { doc, setDoc } from "firebase/firestore";
+import upload from "../../lib/upload";
+import { flushSync } from "react-dom";
 const Login = () => {
   const [avatar, setAvatar] = useState({
     file: null,
     url: "",
   });
 
+  const [loading,setLoading]=useState(false)
   const handleAvatar = (e) => {
     if (e.target.file[0]) {
       setAvatar({
@@ -25,19 +28,19 @@ const Login = () => {
 
   const handleRegister = async (e) => {
     e.preventDefault();
-
+    setLoading(true);
     const formData = new FormData(e.target);
 
     const { username, email, password } = Object.fromEntries(formData);
 
     try {
       const res = await createUserWithEmailAndPassword(auth, email, password);
-      // const img Url
+      // const imgUrl = await upload(avatar.file);
       await setDoc(doc(db, "users", res.user.uid), {
         username,
         email,
         id: res.user.uid,
-        
+        // avatar: imgUrl,
         blocked: [],
       });
       console.log("test -1");
@@ -49,11 +52,26 @@ const Login = () => {
     } catch (err) {
       console.log(err);
       toast.error(err.message);
+    } finally {
+      setLoading(false);
     }
   };
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
-    toast.success("Hello");
+    setLoading(true);
+    const formData = new FormData(e.target);
+
+    const { email, password } = Object.fromEntries(formData);
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      toast.success("login Oke")
+    } catch (err) {
+      console.log(err);
+      toast.error(err.message);
+    } finally {
+      setLoading(false);
+    }
   };
   return (
     <div className="login">
@@ -62,7 +80,7 @@ const Login = () => {
         <form onSubmit={handleLogin}>
           <input type="text" placeholder="Email" name="email" />
           <input type="password" placeholder="Password" name="password" />
-          <button>Sign In</button>
+          <button disabled={loading}> {loading ? "Loading" : "Sign In"}</button>
         </form>
       </div>
       <div className="separator"></div>
@@ -82,7 +100,7 @@ const Login = () => {
           <input type="text" placeholder="Username" name="username" />
           <input type="text" placeholder="Email" name="email" />
           <input type="password" placeholder="Password" name="password" />
-          <button>Sign Up</button>
+          <button  disabled={loading}>{loading ? "Loading" : "Sign Up"}</button>
         </form>
       </div>
     </div>
